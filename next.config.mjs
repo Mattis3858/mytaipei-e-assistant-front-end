f90372da3256dc7f8138bae156c9464ee8bc82c3
Mattis3858 <109306011@nccu.edu.tw>
2025-05-23 14:30:05 +0800

    configure next.js Image with supabase

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "gyvxrvhdtojwnyeedyiz.supabase.co", // Add your Supabase domain here
+      // If you have other external image domains, add them here too
+    ],
+  },
+  // Other Next.js configurations can go here
+};
 
 export default nextConfig;
