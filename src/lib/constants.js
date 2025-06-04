26179a1699455b0caf47eba70d1e97666c16e0d8
Mattis3858 <109306011@nccu.edu.tw>
2025-06-04 14:37:13 +0800

    update announcement page content mx

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -5,4 +5,4 @@ export const BACKEND_API_BASE_URL =
   process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL;
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-export const DEFAULT_USER_ID = "8ffdd9da-cd02-4517-973f-2c0c165aa465";
+export const DEFAULT_USER_ID = "d75e1c85-3fa6-422b-a225-a6695c39943e";
